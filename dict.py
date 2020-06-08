24126bf3c9029f42227d861717127eab018cab1e
Riyazul19 <66468986+Riyazul19@users.noreply.github.com>
2020-06-09 04:23:21 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,7 @@
+# deleting-an-element-from-the-dictionary
+#dictionary can be easily accessible and can be changed whenever we want 
+print("the list of members in family")
+dict1={"ravi":23,"ram":10,"revathy":20}
+print(dict1)
+del dict1["revathy"]
+print("the member list after deleting the name of revathy",dict1)

1d7fbd47e5dda6c40259099d7a76c5e9bc3dc5b0
Riyazul19 <66468986+Riyazul19@users.noreply.github.com>
2020-06-09 04:24:29 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+# assigning-new-element-in-the-different-list
+
+num1=[1,2,3,4]
+num2=[6,7,8]
+print("the merged list is",num1+num2)
+num1[2]="banana"
+print(num1)
+num2.append(9)
+print(num2)
+    
---
+++ tuple.py
@@ -0,0 +1,8 @@
+# accessing-tuple-element
+#tuples are unchanged and ordered so we can access any elements from the tuples but can't change it
+#accesing elemet from the list
+num1=(1,2,3,4,54,6)
+print("number at the first position is ",num1[0])
+print("number at the 4th position is ",num1[3])
+tuple1=("banana","apple","orange")
+print("the first fruit in the fruit list is ",tuple1[0])
